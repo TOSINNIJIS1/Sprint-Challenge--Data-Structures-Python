f79aff40a87b39a09104080d249e9080ac655c58
VINCENT ADENIJI <54925113+TOSINNIJIS1@users.noreply.github.com>
2020-09-28 23:09:04 -0400

    mvp

--- names/names.py
+++ names/names.py
@@ -2,21 +2,33 @@ import time
 
 start_time = time.time()
 
-f = open('names_1.txt', 'r')
+f = open('d:/lambda/Python/Sprint-Challenge--Data-Structures-Python/names/names_1.txt', 'r')
 names_1 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
-f = open('names_2.txt', 'r')
+f = open('d:/lambda/Python/Sprint-Challenge--Data-Structures-Python/names/names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
 duplicates = []  # Return the list of duplicates in this data structure
 
+# names1 = {n for n in names_1}
+names2 = {n for n in names_2}
+
+[duplicates.append(name) for name in names_1 if name in names_2]
+
+
 # Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+
+# for name in names_2:
+#     names2.add_to_head(name)
+#     if names1.contains(name):
+#         duplicates.append(name)
+
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
